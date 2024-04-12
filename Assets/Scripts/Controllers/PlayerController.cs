18769e94460c0b387b7565e3b7a8f2e20ee25d93
Joa25T <jtelleriat@outlook.com>
2024-04-12 17:24:15 -0600

    player warp around screeen

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace StarShooter.Controllers
 {
@@ -12,13 +14,19 @@ namespace StarShooter.Controllers
         [SerializeField] private Vector3 _initialPos = new Vector3 (0,-2,0);
 
         [Header("Movement")]
-        [SerializeField] private float _speed = 5;
+        [SerializeField] private float _speed = 5f;
+        [SerializeField] private float _playerBounds = 9.37f;
 
         //Properties
+        //Movement Properties
         private float ActualSpeed => _speed * Time.deltaTime;
         private float XSpeed => Input.GetAxis("Horizontal")*ActualSpeed;
         private float YSpeed => Input.GetAxis("Vertical")* ActualSpeed;
         private Vector3 MoveDir => new Vector3 (XSpeed,YSpeed,0);
+
+        //Warp Properties
+        private float WarpPosX => transform.position.x > 0 ? -_playerBounds : _playerBounds ;
+        private Vector3 WarpPos => new Vector3 (WarpPosX, transform.position.y, 0);
 
         // Start is called before the first frame update
         void Start()
@@ -32,6 +40,15 @@ namespace StarShooter.Controllers
         {
             //using the old input system to move the player
             Move();
+            WarpPlayer();
+        }
+
+        private void WarpPlayer()
+        {
+            if (MathF.Abs(transform.position.x) > _playerBounds)
+            {
+                transform.position = WarpPos;
+            }
         }
 
         private void Move()
