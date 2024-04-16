80f786eb96a32f355541d57020393c87bf260cd4
Joaquin Telleria <jtelleriat@outlook.com>
2024-04-15 22:12:36 -0600

    Control wrapping and starting on the laser!

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,11 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 namespace StarShooter.Controllers
 {
-    [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
         [Header("Position")]
@@ -13,6 +9,9 @@ namespace StarShooter.Controllers
 
         [Header("Movement")]
         [SerializeField] private float _speed = 5;
+        [SerializeField] private float _topBoundry = 0;
+        [SerializeField] private float _botBoundry = -4.5f;
+        [SerializeField] private float _warpWidth = 9.4f;
 
         //Properties
         private float ActualSpeed => _speed * Time.deltaTime;
@@ -20,8 +19,22 @@ namespace StarShooter.Controllers
         private float YSpeed => Input.GetAxis("Vertical")* ActualSpeed;
         private Vector3 MoveDir => new Vector3 (XSpeed,YSpeed,0);
 
+        //storing the players X position and boundries to stop the player form going out of bounds
+        private Vector3 TopBoundry => new Vector3(transform.position.x, _topBoundry, 0);
+        private Vector3 BotBoundry => new Vector3(transform.position.x, _botBoundry, 0);
+
+        //Warping if the players position is bigger than 1 it needs to go to -_warpWidth and viceversa
+        private float WarpPosX => transform.position.x > 0 ? -_warpWidth : _warpWidth;
+        private Vector3 WarpPos => new Vector3(WarpPosX, transform.position.y, 0);
+
+
         // Start is called before the first frame update
         void Start()
+        {
+            SetInitialPosition();
+        }
+
+        private void SetInitialPosition()
         {
             // asign or reset the player to the inital position when this scene is loaded.
             transform.position = _initialPos;
@@ -32,13 +45,31 @@ namespace StarShooter.Controllers
         {
             //using the old input system to move the player
             Move();
+            Warp();
         }
 
+        private void Warp()
+        {
+            if (Mathf.Abs(transform.position.x)> _warpWidth) 
+            {
+                transform.position = WarpPos;
+            }
+        }
 
         private void Move()
         {
             //Vector3 moveDir = new Vector3(dirX)
             transform.Translate(MoveDir);
+            if(transform.position.y > _topBoundry)
+            {
+                transform.position = TopBoundry;
+                return;
+            }
+            else if(transform.position.y < _botBoundry)
+            {
+                transform.position = BotBoundry; 
+                return;
+            }
         }
     }
 }
